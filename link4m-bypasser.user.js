49b7ef39bc6489bafafddfa2d2749280280684f5
bunviancypher <vpsbun@gmail.com>
2025-11-29 20:18:24 +0700

    Create link4m-bypasser.user.js

---
+++ link4m-bypasser.user.js
@@ -0,0 +1,20 @@
+
+// ==UserScript==
+// @name         Link4M Bypasser
+// @namespace    https://github.com/Bunvian
+// @version      2.0.0
+// @author       Bunvian
+// @match        https://link4m.com/*
+// @grant        GM_addStyle
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
+// @require      https://cdn.jsdelivr.net/gh/Bunvian/link4m-bypasser@main/core.js
+// @run-at       document-idle
+// ==/UserScript==
+
+(function() {
+    if (window.Link4MBypasserCore) {
+        window.Link4MBypasserCore.init(GM_getValue, GM_setValue, GM_addStyle);
+    }
+})();
